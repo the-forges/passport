987b0fcf45ceaad8f63a6365ca405823bff4bb2b
Michael Christenson II <m3talsmith@gmail.com>
2024-04-12 19:20:59 -0400

    Added QR code scanning

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:passport/app/devices.dart';
 import 'profile.dart';
 
@@ -23,6 +27,25 @@ class _AppState extends State<App> {
     });
   }
 
+  String _scanQR = "";
+
+  Future<void> scanQR() async {
+    String barcodeScanRes;
+
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#ff6666', 'Cancel', true, ScanMode.QR);
+      log(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _scanQR = barcodeScanRes;
+    });
+  }
+
   @override
   Widget build(context) {
     return MaterialApp(
@@ -39,7 +62,7 @@ class _AppState extends State<App> {
         ),
         body: _pages.elementAt(_selectedIndex),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: scanQR,
           child: const Icon(Icons.qr_code_scanner_rounded),
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
