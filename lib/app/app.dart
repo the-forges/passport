504fad2dfcb72eceea277343ce7b0ded8874d338
Michael Christenson II <m3talsmith@gmail.com>
2024-04-12 16:39:38 -0400

    Bottom Nav Bar Added

---
+++ lib/app/app.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:passport/app/devices.dart';
+import 'profile.dart';
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int _selectedIndex = 0;
+
+  final _pages = const [
+    Profile(),
+    Devices(),
+  ];
+  
+  _onNavigationBarTap(index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Passport',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+      ),
+      home: Scaffold(
+        body: _pages.elementAt(_selectedIndex),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.qr_code_scanner_rounded),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.person_rounded), label: 'Profile'),
+            BottomNavigationBarItem(icon: Icon(Icons.computer_rounded), label: 'Devices'),
+          ],
+          onTap: _onNavigationBarTap,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/app/devices.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Devices extends StatelessWidget {
+  const Devices({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Devices'),
+    );
+  }
+
+}
